463a2d0f8e3d981c96a0feefb7b3a8eb174f9fc7
Nirbhay Gupta <guptanirbhay93@gmail.com>
2021-10-26 18:56:06 +0530

    [Register] Migration Status

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='oauth-micro-client',
     packages=['oauth_micro_client'],
-    version='0.2.1',
+    version='0.2.2',
     author="Develper Junio",
     author_email='developer@junio.in',
     classifiers=[
