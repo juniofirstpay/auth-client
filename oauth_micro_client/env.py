16a402aa1ae64a6bb34f30e2c569ff6aed55d5cc
Nirbhay Gupta <guptanirbhay93@gmail.com>
2022-12-11 01:03:46 +0530

    [config] custom setup call

--- oauth_micro_client/env.py
+++ oauth_micro_client/env.py
@@ -27,3 +27,29 @@ try:
 
 except Exception as e:
     print(e)
+    
+def setup(env):
+    global __config
+    global __variant
+    try:
+        env = load(open('config.yaml'), Loader=loader)
+        __config = env.get('oauth', {}).get('service')
+        __variant = env.get('oauth', {}).get('variant')
+
+        if __config is None:
+            warn("Config Object Not Found")
+
+        if __config.get('endpoint') is None:
+            warn("oAuth Client Enpoint Not Configured")
+
+        if __config.get('client_id') is None:
+            warn("oAuth Client Id Configured")
+
+        if __config.get('client_secret') is None:
+            warn("oAuth Client Secret Configured")
+
+        if __variant is None:
+            warn("Without variant no call will be made")
+
+    except Exception as e:
+        print(e)
